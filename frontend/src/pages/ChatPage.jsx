cc1954fc2362b16b7a46041153a6857e3be32328
Shubham-12bhatt <bhattshubham466@gmail.com>
2026-02-27 15:56:00 +0530

    chatpage ui change

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -13,9 +13,9 @@ import { useChatStore } from "../store/useChatStore";
 const ChatPage = () => {
   const { activeTab, selectedUser } = useChatStore();
   return (
-    <div className="w-full flex items-center justify-center pt-8 px-4 font-sans overflow-hidden">
+    <div className="w-full min-h-screen flex items-center justify-center p-5 font-sans overflow-hidden">
       {/* Container for the animated border box */}
-      <div className="relative group w-full max-w-6xl h-[800px] rounded-2xl overflow-hidden flex items-center justify-center shadow-2xl">
+      <div className="relative group w-full max-w-[1200px] min-h-[700px] rounded-2xl overflow-hidden flex items-center justify-center shadow-2xl">
         
         {/* Glow behind the border */}
         <div className="absolute w-[200%] h-[200%] bg-[conic-gradient(from_0deg_at_50%_50%,transparent_0%,transparent_35%,#1cd4e5_50%,transparent_65%,transparent_100%)] animate-[spin_4s_linear_infinite] blur-xl opacity-80 group-hover:opacity-100 transition duration-500"></div>
@@ -26,25 +26,25 @@ const ChatPage = () => {
         {/* Inner Card (The actual content container) */}
         <div className="absolute inset-[2px] bg-[#1a2133] rounded-[14px] z-10 flex overflow-hidden">
           
-          {/* left side */}
-          <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col border-r border-slate-700/30">
+          {/* Left Sidebar */}
+          <div className={`w-full md:w-80 lg:w-96 flex-col border-r border-slate-700/30 bg-[#1a2133] z-20 ${selectedUser ? "hidden md:flex" : "flex"}`}>
             <ProfileHeader />
             <ActiveTabSwitch />
-            <div className="flex-1 overflow-y-auto space-y-2 p-4">
-              {activeTab === "chats" ? <ChatsList/> : <ContactsList/>
-              }
+            <div className="flex-1 overflow-y-auto space-y-2 p-4 pt-1">
+              {activeTab === "chats" ? <ChatsList /> : <ContactsList />}
             </div>
           </div>
 
-          {/* right side */}
-          <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
-            {selectedUser ? <ChatContainer /> : <NoConversation/>
-            }
+          {/* Right side */}
+          <div className={`flex-1 flex-col bg-slate-900/50 backdrop-blur-sm relative z-10 ${!selectedUser ? "hidden md:flex" : "flex"}`}>
+            {selectedUser ? <ChatContainer /> : <NoConversation />}
           </div>
           
         </div>
+          
+        </div>
       </div>
-    </div>
+  
   );
 };
 
